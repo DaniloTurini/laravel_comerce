59ed0fdf5c41ea739582c18476bf8092998262e2
Danilo Turini <daniloturini@gmail.com>
2016-01-12 22:22:20 -0200

    PagSeguro 2

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -15,7 +15,7 @@ use PHPSC\PagSeguro\Requests\Checkout\CheckoutService;
 
 class CheckoutController extends Controller
 {
-    public function place(Order $orderModel, OrderItem $orderItem)
+    public function place(Order $orderModel, OrderItem $orderItem, CheckoutService $checkoutService)
     {
         if(!Session::has('cart')){
             return false;
@@ -26,15 +26,20 @@ class CheckoutController extends Controller
 
         if($cart->getTotal() > 0){
 
+            $checkout = $checkoutService->createCheckoutBuilder();
+
             $order = $orderModel->create(['user_id'=>Auth::user()->id, 'total'=>$cart->getTotal()]);
 
             foreach($cart->all() as $k=>$item){
+                $checkout->addItem(new Item($k, $item['name'], number_format($item['price'], 2, ".", ""), $item['qtd']));
                 $order->items()->create(['product_id'=>$k, 'price'=>$item['price'], 'qtd'=>$item['qtd']]);
             }
 
             $cart->clear();
 
-            return view('store.checkout', ['cart'=>'notempty'], compact('order', 'categories'));
+            $response = $checkoutService->checkout($checkout->getCheckout());
+
+            return redirect($response->getRedirectionUrl());
 
         }
 
@@ -51,6 +56,6 @@ class CheckoutController extends Controller
 
         $response = $checkoutService->checkout($checkout);
 
-        redirect($response->getRedirectionUrl());
+        return redirect($response->getRedirectionUrl());
     }
 }
